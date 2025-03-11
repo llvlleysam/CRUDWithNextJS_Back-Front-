39fdb3cd6b085f9f8ffed105a2addc5c72817c68
Meysam Farzalian <meysam.farzalian@gmail.com>
2025-03-11 11:46:46 +0330

    chore: delete comments

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -1,22 +1,25 @@
-import SocialLinks from "@/app/_Components/SocialLinks"
-import { Metadata } from "next"
+import SocialLinks from "@/app/_Components/SocialLinks";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "About page",
-  
-}
+};
 
 export default async function AboutPage() {
-  // await new Promise((resolve) => setTimeout(resolve, 1000))
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <div className="p-4 flex flex-col justify-center">
       <h1>About Me</h1>
       <p className="text-2xl">I am Meysam Farzalian</p>
       <p className="text-xl">I am a junior-level frontend developer.</p>
-      <p className="text-xl">I have been coding and practicing programming for about a year.</p>
-      <p className="text-xl">I was born in 1991 in Iran and currently live in Tehran.</p>
+      <p className="text-xl">
+        I have been coding and practicing programming for about a year.
+      </p>
+      <p className="text-xl">
+        I was born in 1991 in Iran and currently live in Tehran.
+      </p>
       <p className="text-xl">Ways to contact me:</p>
-      <SocialLinks/>
+      <SocialLinks />
     </div>
-  )
+  );
 }
--- app/_Components/WeatherChart.tsx
+++ app/_Components/WeatherChart.tsx
@@ -1,5 +1,5 @@
 export default async function WeatherChart() {
-  // await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
     <div className="p-4 border rounded-xl flex flex-col items-center justify-center gap-2 bg-orange-600/50 animate-pulse">
       <h1 className="text-2xl font-bold">Hello World</h1>
@@ -8,7 +8,10 @@ export default async function WeatherChart() {
         both the frontend and backend with Next.js and connected it to a MongoDB
         database.
       </p>
-      <p>It was quite challenging, but in the end, completing it felt very rewarding.</p>
+      <p>
+        It was quite challenging, but in the end, completing it felt very
+        rewarding.
+      </p>
       <p>I hope to work on better and more powerful projects in the future.</p>
       <p>This was just a simple CRUD application.</p>
       <p className="text-red-500 font-extrabold">Wishing for success!</p>
